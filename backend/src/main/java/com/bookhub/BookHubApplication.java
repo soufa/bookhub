4a8ae587b32fe4f4e01b26dbc9df65afe0832cfb
soufa <mziousofien@gmail.com>
2026-09-22 16:44:13 +0200

    feat: PostgreSQL 5433 + disable Spring Security for dev + Flyway working

--- backend/src/main/java/com/bookhub/BookHubApplication.java
+++ backend/src/main/java/com/bookhub/BookHubApplication.java
@@ -2,8 +2,9 @@ package com.bookhub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class BookHubApplication {
     public static void main(String[] args) {
         SpringApplication.run(BookHubApplication.class, args);
